f208d4e20b90466ca5a4e3d265f38ecced76fc14
AlexxNB <alexxnb@mail.ru>
2021-09-25 16:47:27 +0300

    add routes

--- src/Map.svelte
+++ src/Map.svelte
@@ -2,25 +2,101 @@
     import Map from "@arcgis/core/Map";
     import MapView from "@arcgis/core/views/MapView";
     import esriConfig from "@arcgis/core/config";
+    import FeatureLayer from "@arcgis/core/layers/FeatureLayer";
+    import Graphic from "@arcgis/core/Graphic";
+    import {solve} from "@arcgis/core/rest/route";
+    import RouteParameters from "@arcgis/core/rest/support/RouteParameters";
+    import FeatureSet from "@arcgis/core/rest/support/FeatureSet";
+
 
     import {onMount} from 'svelte';
-    console.log(Map);
+   
 
     esriConfig.apiKey = "AAPK3ace7928316549b28c33b66ea457e676dbsV_3c1DDacVuVpeGLLORLCTguTWMzszmAq_TwOPq10LjIj__CNgFsd1GDFUUHa";
 
-    onMount(()=>{
-        const map = new Map({
-          basemap: "arcgis-topographic" // Basemap layer service
+    const map = new Map({
+        basemap: "arcgis-navigation" //Basemap layer service
     });
 
-    const view = new MapView({
-          map: map,
-          center: [33.981487, 62.267936], // Longitude, latitude
-          zoom: 13, // Zoom level
-          container: "viewDiv" // Div element
+    const flBarrier = new FeatureLayer({
+        portalItem: { // autocasts as esri/portal/PortalItem
+          id: "ab54728e3c794d588a4ea1720db8d10e"
+        }
+    });
+
+    map.add(flBarrier); // adds the layer to the map
+
+    const routeUrl = "https://route-api.arcgis.com/arcgis/rest/services/World/Route/NAServer/Route_World";
+
+
+    onMount(async ()=>{
+        const result = await flBarrier.queryFeatures();
+        const barriers = result.features;
+
+        const view = new MapView({
+            container: "viewDiv",
+            map: map,
+            center: [8.51631, 47.38935], //Longitude, latitude
+            zoom: 14
         });
-    })
-    
+
+        view.on("click", function(event) {
+            if (view.graphics.length === 0) {
+                addGraphic("origin", event.mapPoint);
+            } else if (view.graphics.length === 1) {
+                addGraphic("destination", event.mapPoint);
+                getRoute(); // Call the route service
+            } else {
+                view.graphics.removeAll();
+                addGraphic("origin", event.mapPoint);
+            }
+        });
+
+        function addGraphic(type, point) {
+            const graphic = new Graphic({
+            symbol: {
+                type: "simple-marker",
+                color: (type === "origin") ? "white" : "black",
+                size: "8px"
+            },
+            geometry: point
+            });
+            view.graphics.add(graphic);
+        }
+
+        async function getRoute() {
+            console.log(barriers);
+            const routeParams = new RouteParameters({
+                stops: new FeatureSet({
+                    features: view.graphics.toArray()
+                }),
+                returnDirections: true,
+                polygonBarriers: barriers
+            });
+            const data = await solve(routeUrl, routeParams);
+
+            data.routeResults.forEach(function(result) {
+                result.route.symbol = {
+                    type: "simple-line",
+                    color: [5, 150, 255],
+                    width: 3
+                };
+
+                view.graphics.add(result.route);
+            });
+                
+        }
+        
+    });
+
+
+   
+      
+      
+      
+      
+      
+      
 </script>
 
 <div id="viewDiv" style="height: 100vh;"></div>
